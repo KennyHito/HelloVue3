14f48b7b88dd1a6d52bf5bb514e0688c0bb791ac
yuhaitao <yuhaitao1@lczq.com>
2025-04-22 16:28:29 +0800

    modify: 更新

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
 <template>
   <div class="app-box">
     <span>你好,世界!</span>
+    <Test />
   </div>
 </template>
 
 <!-- 逻辑处理 -->
 <script lang="ts">
 // 引入其他组件
-
+import Test from "@/components/Test.vue"
 export default {
+  components: {
+    Test
+  },
   name: 'App',
 }
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<!-- 结构 -->
+<template>
+  <div>{{ a }}</div>
+</template>
+
+<!-- 逻辑处理 -->
+<script lang="ts" name="Test" setup>
+// 引入其他组件
+import { ref } from 'vue'
+
+let a = ref(100)
+
+</script>
+
+
+<!-- 样式 -->
+<style scoped lang='less'></style>
