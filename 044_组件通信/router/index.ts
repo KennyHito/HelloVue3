3659c480b2c60f78e315d0e58d2c9896575d9cd6
yuhaitao <yuhaitao1@lczq.com>
2025-04-25 09:09:48 +0800

    modify: 更新

--- 044_组件通信/router/index.ts
+++ 044_组件通信/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Props from '@/pages/01_props/Father.vue'
-import Event from '@/pages/02_custom-event/Father.vue'
-import Bus from '@/pages/03_mitt/Father.vue'
-import Model from '@/pages/04_v-model/Father.vue'
-import AttrsListeners from '@/pages/05_$attrs/Father.vue'
-import RefChildrenParent from '@/pages/06_$refs-$parent/Father.vue'
-import ProvideInject from '@/pages/07_provide-inject/Father.vue'
-import Pinia from '@/pages/08_pinia/Father.vue'
-import Slot from '@/pages/09_slot/Father.vue'
+import Props from '../pages/01_props/Father.vue'
+import Event from '../pages/02_custom-event/Father.vue'
+import Bus from '../pages/03_mitt/Father.vue'
+import Model from '../pages/04_v-model/Father.vue'
+import AttrsListeners from '../pages/05_$attrs/Father.vue'
+import RefChildrenParent from '../pages/06_$refs-$parent/Father.vue'
+import ProvideInject from '../pages/07_provide-inject/Father.vue'
+import Pinia from '../pages/08_pinia/Father.vue'
+import Slot from '../pages/09_slot/Father.vue'
 
 export default createRouter({
 	history: createWebHistory(),
