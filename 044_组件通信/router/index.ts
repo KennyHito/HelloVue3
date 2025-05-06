aeec492c9ab89f54d4fc8e998a559e812e8ca65b
yuhaitao <yuhaitao1@lczq.com>
2025-05-06 11:16:26 +0800

    modify: 更新

--- 044_组件通信/router/index.ts
+++ 044_组件通信/router/index.ts
@@ -8,8 +8,8 @@ import RefChildrenParent from '../pages/06_$refs-$parent/Father.vue'
 import ProvideInject from '../pages/07_provide-inject/Father.vue'
 import Pinia from '../pages/08_pinia/Father.vue'
 import Slot1 from '../pages/09_slot_默认插槽/Father.vue'
-import Slot2 from '../pages/09_slot_具名插槽/Father.vue'
-import Slot3 from '../pages/09_slot_作用域插槽/Father.vue'
+import Slot2 from '../pages/10_slot_具名插槽/Father.vue'
+import Slot3 from '../pages/11_slot_作用域插槽/Father.vue'
 
 export default createRouter({
 	history: createWebHistory(),
