5bfa564d6c79cd9c15e69a3a98f8f909c3aeb14a
yuhaitao <yuhaitao1@lczq.com>
2025-05-14 16:42:27 +0800

    modify: 更新

--- 048_customRef/App.vue
+++ 048_customRef/App.vue
@@ -6,25 +6,28 @@
 </template>
 
 <script setup lang="ts" name="App">
-	import {ref} from 'vue'
-	import useMsgRef from './useMsgRef'
+import { ref } from 'vue'
+import useMsgRef from './useMsgRef'
 
-	// 使用Vue提供的默认ref定义响应式数据，数据一变，页面就更新
-	// let msg = ref('你好')
+// 使用Vue提供的默认ref定义响应式数据，数据一变，页面就更新
+// let msg = ref('你好')
 
-	// 使用useMsgRef来定义一个响应式数据且有延迟效果
-	let {msg} = useMsgRef('你好',2000)
+// 使用useMsgRef来定义一个响应式数据且有延迟效果
+// 1、track  作用：它的功能是追踪某个依赖。当响应式数据被读取时，就会触发依赖收集操作。
+// 2、trigger 作用：当响应式数据被修改时，借助 trigger 可以通知所有依赖该数据的地方进行更新。
+let { msg } = useMsgRef('你好', 2000)
 
 </script>
 
 <style scoped>
-	.app {
-		background-color: #ddd;
-		border-radius: 10px;
-		box-shadow: 0 0 10px;
-		padding: 10px;
-	}
-	button {
-		margin:0 5px;
-	}
+.app {
+	background-color: #ddd;
+	border-radius: 10px;
+	box-shadow: 0 0 10px;
+	padding: 10px;
+}
+
+button {
+	margin: 0 5px;
+}
 </style>
